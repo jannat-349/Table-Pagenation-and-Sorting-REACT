9a9164ddb63e0a91134ecc23df28b0257b48345d
jannat28-349 <jucse2.349@gmail.com>
2023-03-12 23:41:18 +0600

    sorting done with some css

--- pagenation-sorting-react/src/entries/Entries.jsx
+++ pagenation-sorting-react/src/entries/Entries.jsx
@@ -6,6 +6,7 @@ function Entries() {
   const [entrySerial, setEntrySerial] = useState(0);
   const [pageNo, setPageNo] = useState(1);
   const [isLoading, setIsLoading] = useState(false);
+  const [state, setState] = useState("");
   useEffect(() => {
     // console.log("effect hook triggered");
     const fetchData = async () => {
@@ -14,6 +15,9 @@ function Entries() {
         const res = await fetch("https://api.publicapis.org/entries");
         const data = await res.json();
         setEntries(data.entries.slice(entrySerial, entrySerial + 10));
+        setState("ogochalo");
+
+        // console.log(entries.state);
       } catch (err) {
         alert("failed to load data");
         console.error(err);
@@ -21,7 +25,6 @@ function Entries() {
         setIsLoading(false);
       }
     };
-
     fetchData();
   }, [entrySerial]);
 
@@ -38,20 +41,50 @@ function Entries() {
     return <div>Loading...</div>;
   }
 
+  function sort__entries() {
+    // setIsLoading(true);
+    if (state === "ogochalo" || state === "descending") {
+      setEntries((entries) =>
+        entries.sort((entry_1, entry_2) => {
+          return entry_1.Description > entry_2.Description ? 1 : -1;
+        })
+      );
+      //   console.log(entries);
+      // entries.sort();
+      setState("ascending");
+    } else if (state === "ascending") {
+      setEntries((entries) =>
+        entries.sort((entry_1, entry_2) => {
+          return entry_1.Description < entry_2.Description ? 1 : -1;
+        })
+      );
+      setState("descending");
+      // setEntries((entries) => entries
+      // .sort((a,b) => b.Description - a.Description));
+    }
+  }
+
   return (
     <div className="entries__output">
       {/* {console.log("rendered")} */}
       <table className="entry__tbl">
         <caption>Entry Table</caption>
         <thead>
-          <th>API</th>
-          <th>Description</th>
-          <th>Category</th>
+          <tr>
+            <th>
+              Description
+              {state === "ogochalo" || state === "descending" ? (
+                <button onClick={sort__entries} className = "sort__btn">Ascending Sort</button>
+              ) : (
+                <button onClick={sort__entries} className = "sort__btn">Descending Sort</button>
+              )}
+            </th>
+            <th>Category</th>
+          </tr>
         </thead>
         <tbody>
           {entries.map((entry, index) => (
             <tr key={index}>
-              <td>{entry.API}</td>
               <td>{entry.Description}</td>
               <td>{entry.Category}</td>
             </tr>
