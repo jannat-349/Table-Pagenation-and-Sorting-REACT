13e242a46a613539890d6fc376e677712df472eb
jannat28-349 <jucse2.349@gmail.com>
2023-03-12 16:11:25 +0600

    table, css, page no added

--- pagenation-sorting-react/src/entries/Entries.jsx
+++ pagenation-sorting-react/src/entries/Entries.jsx
@@ -4,6 +4,7 @@ import "./entries.css";
 function Entries() {
   const [entries, setEntries] = useState([]);
   const [entrySerial, setEntrySerial] = useState(0);
+  const [pageNo, setPageNo] = useState(1);
   useEffect(() => {
     // console.log("effect hook triggered");
     fetch("https://api.publicapis.org/entries")
@@ -20,26 +21,48 @@ function Entries() {
 
   function showNext() {
     setEntrySerial((entrySerial) => entrySerial + 10);
+    setPageNo((pageNo) => pageNo+1);
   }
   function showPrevious() {
     setEntrySerial((entrySerial) => entrySerial - 10);
+    setPageNo((pageNo) => pageNo-1);
   }
 
   return (
     <div className="entries__output">
-      {console.log("rendered")}
-      <ul>
-        {entries.map((entry, index) => (
-          <li key={index}>{entry.Description}</li>
-        ))}
-      </ul>
-      <button onClick={showNext} className="next__btn">
-        Next Page
-      </button>
-      {(entrySerial >= 10) ? (
-        <button onClick={showPrevious} className="next__btn">
-        Previous Page
-      </button>) : <></>}
+      {/* {console.log("rendered")} */}
+      <table className="entry__tbl">
+        <caption>Entry Table</caption>
+        <thead>
+          <th>API</th>
+          <th>Description</th>
+          <th>Category</th>
+        </thead>
+        <tbody>
+          {entries.map((entry, index) => (
+            <tr key={index}>
+              <td>{entry.API}</td>
+              <td>{entry.Description}</td>
+              <td>{entry.Category}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="button__pageNo">
+        <div className="btn">
+          <button onClick={showNext} className="next__btn">
+            Next Page
+          </button>
+          {entrySerial >= 10 ? (
+            <button onClick={showPrevious} className="next__btn">
+              Previous Page
+            </button>
+          ) : (
+            <></>
+          )}
+        </div>
+        <div>Page no: {pageNo}</div>
+      </div>
     </div>
   );
 }
